51e04bfe9d2c5e020487927c41e3ddb68ad0168e
Grantjos12 <grantjoslyn@gmail.com>
2020-10-19 19:03:09 -0500

    made Engineer and Intern classes

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,16 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+
+const employee = require("./Employee.js");
+
+class Engineer extends employee {
+  constructor(github) {
+    super(name, id, email);
+    this.github = github;
+  }
+  getGithub() {
+    return this.github;
+  }
+  getRole() {
+    return "Engineer";
+  }
+}
--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -1,3 +1,14 @@
 // TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
 
 const employee = require("./Employee.js");
+
+//extends to link to Employee class
+class Manager extends employee {
+  constructor(officeNumber) {
+    super(name, id, email); ///super add the parameters from Employee class
+    this.officeNumber = officeNumber;
+  }
+  getRole() {
+    return "Manager";
+  }
+}
